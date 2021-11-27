6d1e519858192ef09f79c054b432b570075c8cec
KushKim <ds12000@naver.com>
2021-11-27 10:58:30 +0900

    ui script speedmeter script start scenemanager

---
+++ asdftrain/Assets/speedmeter.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class speedmeter : MonoBehaviour
+{
+    Rigidbody rigd;
+    private Vector3 m_LastPosition;
+    public float m_Speed;
+    public Text m_KilometersPerHour;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigd = GetComponent<Rigidbody>();
+
+    }
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        m_Speed = GetSpeed();
+        m_KilometersPerHour.text = string.Format("{0:00} km/h", m_Speed * 3.6f);
+        if (Input.GetKey("up"))
+        {
+            rigd.velocity = new Vector3(0, 0, 10);
+        }
+
+    }
+    float GetSpeed()
+    {
+        float speed = (((transform.position - m_LastPosition).magnitude) / Time.deltaTime);
+        m_LastPosition = transform.position;
+
+        return speed;
+    }
+}
